ee04b704ab8655d60503b8764bad293825058391
Ron Vergis <ron.vergis@gmail.com>
2026-02-04 02:48:08 +0000

    update scripts

---
+++ scripts/telegram-send-test-message.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://api.telegram.org/bot8227473482:AAGfvptxfONHscGzCMZImAVVJP8ahqbcvHY/sendMessage" \
+  -d chat_id=7516696773 \
+  -d text="Test successful! Clawd bot can send messages to Ron (ID 7516696773)."
